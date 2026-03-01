2ca311fa77cffc7e5a006e3ac3aa06d5e62dd20c
Satabdo Majumder <satabdomajumdertech@gmail.com>
2026-03-01 20:15:45 +0530

    Create, delete product endpoints

---
+++ src/main/java/com/example/FakeCommerce/controllers/ProductController.java
@@ -0,0 +1,32 @@
+package com.example.FakeCommerce.controllers;
+
+import com.example.FakeCommerce.dtos.CreateProductRequestDto;
+import com.example.FakeCommerce.schema.Product;
+import com.example.FakeCommerce.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody CreateProductRequestDto requestDto) {
+        System.out.println("Here");
+        return productService.createProduct(requestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+    }
+}
--- src/main/java/com/example/FakeCommerce/services/ProductService.java
+++ src/main/java/com/example/FakeCommerce/services/ProductService.java
@@ -1,5 +1,6 @@
 package com.example.FakeCommerce.services;
 
+import com.example.FakeCommerce.dtos.CreateProductRequestDto;
 import com.example.FakeCommerce.repositories.ProductRepository;
 import com.example.FakeCommerce.schema.Product;
 import lombok.RequiredArgsConstructor;
@@ -13,8 +14,24 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public List<Product> getAllProducts(){
+    public List<Product> getAllProducts() {
         return productRepository.findAll();
+    }
+
+    public Product createProduct(CreateProductRequestDto requestDto) {
+        Product newProdict = Product.builder()
+                .title(requestDto.getTitle())
+                .description(requestDto.getDescription())
+                .price(requestDto.getPrice())
+                .image(requestDto.getImage())
+                .category(requestDto.getCategory())
+                .rating(requestDto.getRating())
+                .build();
+        return productRepository.save(newProdict);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
     }
 
 }
