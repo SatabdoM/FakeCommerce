857213293c5698b3622ea1b605a0fc452e4cce63
Satabdo Majumder <satabdomajumdertech@gmail.com>
2026-03-07 20:59:03 +0530

    Added CreatedAt, UpdatedAt using EntityListener

--- src/main/java/com/example/FakeCommerce/FakeCommerceApplication.java
+++ src/main/java/com/example/FakeCommerce/FakeCommerceApplication.java
@@ -2,8 +2,10 @@ package com.example.FakeCommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing  //Enables JPA Auditing, which allows for automatic population of auditing fields like createdAt and updatedAt in entities that extend BaseEntity.
 public class FakeCommerceApplication {
     public static void main(String[] args) {
         SpringApplication.run(FakeCommerceApplication.class, args);
--- src/main/java/com/example/FakeCommerce/schema/BaseEntity.java
+++ src/main/java/com/example/FakeCommerce/schema/BaseEntity.java
@@ -2,13 +2,27 @@ package com.example.FakeCommerce.schema;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Data
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedDate;
 }
 //We do not want a table of base entity
 //We want to use the id field in the base entity for all the entities that extend it
